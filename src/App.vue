81dce03c0b79c7ec95a5be8931ffa6fe4daf9c61
ThomasChaures <103081112+ThomasChaures@users.noreply.github.com>
2024-09-18 23:45:54 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+  name: 'App',
+  components: { RouterLink, RouterView }
+}
+</script>
+
+<template>
+  <main>
+    <RouterView />
+  </main>
+</template>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'AppNavbar'
+}
+</script>
+
+<template>
+  <header class="container p-4 mx-auto">
+    <div class="mx-auto flex flex-col"></div>
+  </header>
+</template>
