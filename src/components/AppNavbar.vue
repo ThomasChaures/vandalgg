eb9f67d7b5787add7ef39bf201f35f1552c2b566
ThomasChaures <103081112+ThomasChaures@users.noreply.github.com>
2024-09-23 19:39:17 -0300

    Add files via upload

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -33,7 +33,7 @@ export default {
 </script>
 
 <template>
-  <header class="fixed top-0 left-[300px] h-full  w-[300px] p-4 pl-7 bg-slate-900 flex flex-col justify-between">
+  <header class="absolute top-0 left-0 h-[900px] w-[300px] p-4 pl-7 bg-slate-900 flex flex-col justify-between">
     <div>
       <p class="text-4xl text-white font-semibold tracking-wider uppercase mb-10">Vandal</p>
     </div>
--- src/components/chatGlobal/chatGlobalSection.vue
+++ src/components/chatGlobal/chatGlobalSection.vue
@@ -38,11 +38,11 @@ export default {
       <template v-else>
         <div class="relative h-screen">
           <div class="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <div class="animate-spin rounded-full h-20 w-20 border-4 border-blue-500"></div>
+            <div class="animate-spin rounded-full h-20 w-20  border-t-2 border-b-2 border-blue-500"></div>
           </div>
           <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <div
-              class="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-slate-900"
+              class="animate-spin rounded-full h-20 w-20 border-slate-900"
             ></div>
           </div>
         </div>
